88eafc46f10168119cb4da95eb789791cd316690
Iskander <ika17@sfu.ca>
2024-10-13 16:09:55 -0700

    Set the keys to control the players movement

--- code/main.py
+++ code/main.py
@@ -18,7 +18,8 @@ surf.fill ("yellow")
 # importing an image 
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha() # "conver.alpha" - allows to improve performances of surfaces dramatically (more frame rates)
 player_rect = player_surf.get_frect(midbottom = (WINDOW_WIDTH /2 , WINDOW_HEIGHT - 200))
-player_direction = 1
+player_direction = pygame.math.Vector2() # As the default should be 0 and 0
+player_speed = 300
 
 meteor_surf = pygame.image.load (join('images', 'meteor.png')).convert_alpha()
 meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
@@ -30,13 +31,28 @@ star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, 1280), randint(0,720)) for i in range (20)]
 
 while running: 
-    clock.tick(20)
-    # event loop 
+    dt = clock.tick() /1000
+    # Event loop 
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             running = False
+        #if event.type == pygame.KEYDOWN and event.key == pygame.K_1: /// I did this as a comment, since I realized that there better way to do player movement than in the loop (outside of the loop obviously)
+            #print(1)                                                 /// That's because if I am going to hold any button I will only get one output
+        #if event.type == pygame.MOUSEMOTION:                         /// However, If I do it outside of the loop I will get output as long as I hold the buttons
+            #player_rect.center = event.pos
             
-    # draw the game 
+
+    # Input
+    #pygame.mouse.get_pos()
+    keys = pygame.key.get_pressed()
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT]) # This is the proper way to set the to control the player motion
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    player_direction = player_direction.normalize() if player_direction else player_direction # This code is to normalize the speed of the player when he moves diagonally (it used to be faster)
+    player_rect.center += player_direction * player_speed * dt
+    
+    
+            
+    # Draw the game 
     display_surface.fill('darkgrey') # to fill the space in blue
     for pos in star_positions:
         display_surface.blit(star_surf, pos)
@@ -46,11 +62,7 @@ while running:
     display_surface.blit(laser_surf, laser_rect)
     display_surface.blit(player_surf, player_rect.topleft) # code to put one surface (The player.png in this case) on another surface (Origin point is on the top left)
 
-      # Player movement
-    player_rect.x += 20
 
-           
-  
     pygame.display.update()
     
     
